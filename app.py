14a958ab6326a63b2592fc298e8c4c06e59d28c7
Zachary M. Jones <zmj@zmjones.com>
2014-02-15 10:26:14 -0500

    adds minimal example

---
+++ app.py
@@ -0,0 +1,31 @@
+import sys
+from flask import Flask, render_template
+from flask_flatpages import FlatPages
+from flask_frozen import Freezer
+
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+flatpages = FlatPages(app)
+freezer = Freezer(app)
+app.config['FREEZER_BASE_URL'] = ''
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', pages=flatpages)
+
+
+@app.route('/<path:path>')
+def page(path):
+    page = flatpages.get_or_404(path)
+    return render_template('page.html', page=page)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(port=8000)
